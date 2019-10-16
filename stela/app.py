f9668673f75aa6319039dc9b4740624f8b442f39
Gustavo <gustavo.silva6785@gmail.com>
2019-10-16 02:07:27 -0300

    Inicio do projeto

--- stela/app.py
+++ stela/app.py
@@ -1,16 +1,7 @@
 import spacy
 
 classification_tags = {
-  'reminder': ['lembrar', 'recordar', 'memorizar', 'memorar']
+  'reminder': ['onde', 'qual', 'oque', 'quem']
 }
 
-nlp = spacy.load('pt')
-
-print('Start conversation.\n')
 message = input()
-
-tokenized_message = nlp(message)
-
-for token in tokenized_message:
-  print(token.orth_, token.pos_, token.ent_type_)
-      
