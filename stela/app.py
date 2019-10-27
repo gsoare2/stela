e15ffc7185dc8a6b0961529568c9cca1edd7fdf8
Gustavo <gustavo.silva6785@gmail.com>
2019-10-27 00:15:11 -0300

    store function ready

--- stela/app.py
+++ stela/app.py
@@ -1,80 +1,9 @@
-import nltk 
-from nltk.stem import RSLPStemmer
+import spacy
+from utils.functions import normalize_sentence, get_all
 
-nltk.download('stopwords')
+nlp = spacy.load("pt")
 
-sentence = "A chave está na caixa azul"
-
-def tokenize(sentence):
-  sentence = sentence.lower()
-  sentence = nltk.word_tokenize(sentence)
-  return sentence
-
-def stemming(sentence):
-  stemmer = RSLPStemmer()
-  phrase = []
-  for word in sentence:
-    phrase.append(stemmer.stem(word.lower()))
-  return phrase
-
-def remove_stop_words(sentence):
-  stopwords = nltk.corpus.stopwords.words('portuguese')
-  phrase = []
-  for word in sentence:
-    if word not in stopwords:
-      phrase.append(word)
-  return phrase
-
-def train():
-  training_data = []
-  training_data.append({"class": "amor", "phrase": "Eu te amo"})
-  training_data.append({"class": "amor", "phrase": "Eu gosto de você"})
-  training_data.append({"class": "amor", "phrase": "Você é o amor da minha vida"})
-  training_data.append({"class": "medo", "phrase": "estou com medo"})
-  training_data.append({"class": "medo", "phrase": "tenho medo de fantasma"})
-  training_data.append({"class": "localizacao", "phrase": "a marmita está na mesa"})
-  training_data.append({"class": "localizacao", "phrase": "a bola está na sala"})
-  print("%s frases incluidas" % len(training_data))
-  return training_data
-
-def learning(training_data):
-  corpus_words = {}
-  for data in training_data:
-    frase = data['phrase']
-    frase = tokenize(frase)
-    frase = stemming(frase)
-    frase = remove_stop_words(frase)
-    class_name = data['class']
-    if class_name not in list(corpus_words.keys()):
-      corpus_words[class_name] = {}
-    for word in frase:
-      if word not in list(corpus_words[class_name].keys()):
-        corpus_words[class_name][word] = 1
-      else:
-        corpus_words[class_name][word] += 1
-  return corpus_words
-
-data = train()
-data = learning(data)
-
-def calculate_class_score(sentence,class_name):
-  score = 0 
-  sentence = tokenize(sentence)
-  sentence = stemming(sentence)
-  for word in sentence:
-    if word in data[class_name]:
-      score += data[class_name][word]
-  return score
-
-def calculate_score(sentence):
-  high_score = 0
-  classname = 'default'
-  for classe in data.keys():
-    pontos = 0
-    pontos = calculate_class_score(sentence,classe)
-    if pontos > high_score:
-      high_score = pontos
-      classname = classe
-  return classname, high_score
-
-print(calculate_score("meu tênis está no meu quarto"))
+sentence = 'me lembre que o nome do meu neto é joão'
+message = nlp(sentence)
+normalize_sentence(message, sentence)
+print(get_all())
--- stela/utils/functions.py
+++ stela/utils/functions.py
@@ -1,8 +1,37 @@
-ignore_words = ['a', 'e', 'na']
+store_tokens = ['lembrar', 'registrar', 'guardar', 'armazenar', 'memorizar', 'recordar', 'relembrar', 'decorar', 'fixar', 'gravar', 'arquivar', 'salvar']
+ignore_words = ['o', 'do', 'da', 'de', 'e', 'a', 'me', 'na', 'em', ' ', 'que', 'é', 'meu']
+recover_tokens = ['onde', 'qual', 'quando', 'quem', 'oque', 'porque']
+db = []
 
-def cleanPhrase(phrase):
+def clean_phrase(phrase):
   _phrase = phrase.split()
+  _remove_words = []
   for _t in _phrase:
     if _t in ignore_words:
-      _phrase.remove(_t)
+      _remove_words.append(_t)
+  [_phrase.remove(_s) for _s in _remove_words]
   return " ".join(_phrase)
+
+def normalize_sentence(sentence, sentence_untokenized):
+  action, index = find_root_verb(sentence, sentence_untokenized)
+  predicate = find_predicate(sentence_untokenized, (index + len(action)))
+  completed_phrase = sentence_untokenized
+
+  store(action, predicate, completed_phrase, "10/10/10")
+
+def find_root_verb(sentence, sentence_untokenized):
+  for _word in sentence:
+    if _word.pos_ == "VERB" and _word.dep_ == "ROOT" and _word.lemma_ in store_tokens:
+      return _word, sentence_untokenized.index(_word.text)
+
+def find_predicate(sentence_untokenized, index):
+  pattern_slice = slice(index, len(sentence_untokenized), 1)
+  predicate = sentence_untokenized[pattern_slice]
+  predicate = clean_phrase(predicate.lower())
+  return predicate
+
+def store(action, predicate, completed_phrase, date):
+  db.append({"action": action, "predicate": predicate, "completed_phrase": completed_phrase, "date": date})
+
+def get_all():
+  return db
